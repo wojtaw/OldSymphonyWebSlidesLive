877159da119d89b8e4336c9e3f4d6205268d55f6
Vojta Ciml <VojtaCiml@gmail.com>
2012-07-18 12:59:03 +0200

    Hello SlidesLive homepage created

--- src/SlidesLive/SlidesLiveBundle/Controller/DefaultController.php
+++ src/SlidesLive/SlidesLiveBundle/Controller/DefaultController.php
@@ -3,28 +3,15 @@
 namespace SlidesLive\SlidesLiveBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use SlidesLive\SlidesLiveBundle\Controller\BaseController;
 
-class DefaultController extends BaseController { 
-    
-    public function __construct() {
-      parent::__construct();    
-    }
+class DefaultController extends Controller { 
+
                                               
-    public function indexAction() {
-        $this->start();
-        $em = $this->getDoctrine()->getEntityManager();
-        $session = $this->get('session');
-        $this->data = array('presentations' => array());
-        
-        // FIX ME - predelat na timeline
-        $this->data['presentations'] = $em->getRepository('MetaBundle:Presentation')->list18NewestPresentations($session->get('accessControl'));
-        if (count($this->data['presentations']) < 1) {
-            $session->setFlash('notice','There are no available presentations now.');
-            return $this->render('MetaBundle:Default:index.html.twig', $this->data);           
-        }
-        
-        return $this->render('MetaBundle:Default:index.html.twig', $this->data);               
+    public function indexAction()
+    {
+        return $this->render('SlidesLiveBundle:Homepage:index.html.twig');
     }
     
                      

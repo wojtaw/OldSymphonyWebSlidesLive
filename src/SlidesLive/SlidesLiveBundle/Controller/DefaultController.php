cbe49bfbfdc40b1559140bbbe429313bbd528628
Petr Mrakava <p.mrakava@centrum.cz>
2012-08-30 19:35:58 +0200

    Player page.

--- src/SlidesLive/SlidesLiveBundle/Controller/DefaultController.php
+++ src/SlidesLive/SlidesLiveBundle/Controller/DefaultController.php
@@ -11,11 +11,11 @@ class DefaultController extends Controller {
 
   protected $data = array(
     //'privacy_mode' => Privacy::p_public(),
-    'stylesheet' => null,
-    'account' => null,
-    'presentation' => null,
-    'folder' => null,
-    'folderPresentations' => array(),
+    'stylesheet' => null,       // individualni styl accountu
+    'account' => null,      
+    'presentation' => null,     // prezentace pro prehrávac
+    'folders' => array(),       // seznam folderu kanálu
+    'folderPresentations' => array(),   // prezentace aktuálního folderu
   );
 
   public function accountAction($accountCanName) {
@@ -26,6 +26,17 @@ class DefaultController extends Controller {
       return $this->redirect($this->generateUrl('errorPage', array('message' => "We are sorry, but $accountCanName does not exist.")));
     }
     $this->data['account'] = $this->data['account'][0];
+    $context = $this->get('security.context');
+    if ($context->isGranted('ROLE_USER')) {
+      if ($context->getToken()->getUser()->getId() == $this->data['account']->getId()) {
+        $this->data['folders'] = $this->data['account']->getFolders();
+        $this->data['folderPresentations'] = $this->data['account']->getPrimaryFolder()->getPresentations();
+        if (count($this->data['folderPresentations']) > 0) {
+          $this->data['presentation'] = $this->data['folderPresentations'][0];
+        }
+      }      
+    }
+      
      
     return $this->render('SlidesLiveBundle:Default:playerPage.html.twig', $this->data);
   }
@@ -38,6 +49,20 @@ class DefaultController extends Controller {
   public function presentationAction($accountCanName, $folderCanName, $presentationId) {
     return new Response("Controller:Default , Action: presentation,<br />\$accountCanName: $accountCanName,<br />\$folderCanName: $folderCanName,<br />\$presentationId: $presentationId"); 
     //return $this->render('');
+  }
+  
+// ----------------HELP METHODS-------------------------------------------------
+
+  public function prepareFolders() {
+  
+  }
+  
+  public function prepareFolderPresentations() {
+    
+  }
+  
+  public function includeStylesheet(Account $account) {
+      
   }
                                                                   
 }

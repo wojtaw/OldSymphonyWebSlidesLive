75933adf201ad041dc77987f9cc0141bf95c86d8
Petr Mrakava <p.mrakava@centrum.cz>
2012-09-18 13:20:15 +0200

    Change password controller.

--- src/SlidesLive/SlidesLiveBundle/Controller/TestController.php
+++ src/SlidesLive/SlidesLiveBundle/Controller/TestController.php
@@ -20,5 +20,23 @@ class TestController extends Controller {
       }      
       return $this->render('SlidesLiveBundle:Test:linkBrowser.html.twig', $this->data);   
     }
-                     
+
+    public function passwordChangeAction($accountCanName, $password) {
+      $em = $this->getDoctrine()->getEntityManager();
+      $results = $em->getRepository('SlidesLiveBundle:Account')->findByCanonicalName($accountCanName);
+      if (count($results) != 1) {
+        print("Account not found.<br />\n");        
+      }
+      else {
+        $account = $results[0];
+        print("Accont: ".$account->getName()."<br />\n");
+        print("Password: $password<br />\n");
+        $encoder = $this->get('security.encoder_factory')->getEncoder($account);
+        $account->setPassword($encoder->encodePassword($password, $account->getSalt()));
+        print("Password created.<br />\n");                
+        $em->flush();
+        print("Password saved.<br />\n");        
+      }
+      return new Response('');
+    }                     
 }

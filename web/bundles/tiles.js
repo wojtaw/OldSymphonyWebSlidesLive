2e6376308b26d556ddfda200ba8052288f33019c
Vojta Ciml <VojtaCiml@gmail.com>
2012-10-22 20:13:13 +0200

    Tiles framework basics ready

--- web/bundles/tiles.js
+++ web/bundles/tiles.js
@@ -2,26 +2,54 @@
    console.log("jes");
  });
  
+$(window).resize(resizePresentations); 
+ 
+ 
 
+ 
+//Objects and variables
 function presentationObject(presentationName,presentationImage)
 {
 	this.presentationName=presentationName;
 	this.presentationImage=presentationImage;
 }
 
+var minimumTileWidth = 200;
+var tileSpacing = 6;
+
+
 var presentations = new Array();
-
-
 var elementsString = "";
+
+
+//functions
+function resizePresentations(){
+		var containerWidth = $('#boxWrapper').width()-1;
+		var numberOfPresentations = Math.floor(containerWidth / (tileSpacing + minimumTileWidth));
+		var newTileWidth = Math.floor(containerWidth / numberOfPresentations - tileSpacing);
+		console.log(newTileWidth);
+		resizeTiles(newTileWidth);
+}
+
+function resizeTiles(newTileWidth){
+	$('.box_1_1').css("width", newTileWidth);
+	$('.box_1_1').css("height", newTileWidth);
+	$('.box_1_1').css("margin-right", tileSpacing);		
+	$('.box_1_1').css("margin-bottom", tileSpacing);			
+}
+
 
 function createElements(){
 	loadPresentationData();
-	
+
 	for (var i=0; i<presentations.length; i++){
+		
+		
 		elementsString += "<div class=\"box_1_1\" id=\""+presentations[i].presentationName+"\" style=\"background-color:\#"+Math.floor(Math.random()*16777215).toString(16)+"\"></div>\n";
 	}	
 	
 	console.log(elementsString);
+		console.log($('#boxWrapper').width());
 	$('#boxWrapper').html(elementsString);
 }
 
@@ -33,7 +61,7 @@ function loadPresentationData(){
 //BIG FAKE PART -----------------------------------------------------------------
 //Debug functions
 function createRandomPresentations(){
-	for (var i=0; i<6; i++){
+	for (var i=0; i<60; i++){
 		tmpPresentation = presentationObject;
 		presentations[i] = new presentationObject(makeName(), "prdel.jpg");
 	}

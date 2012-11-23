c7706c4f73ac4e3d0945d43483fc4c7af85312aa
vojta <vojta.d@gmail.com>
2012-11-24 00:10:25 +0100

    * return correct http status code on error page (404)

--- src/SlidesLive/SlidesLiveBundle/Controller/ErrorController.php
+++ src/SlidesLive/SlidesLiveBundle/Controller/ErrorController.php
@@ -10,27 +10,26 @@ Error codes
 1001 - Category does not exists
 */
 
+class ErrorController extends Controller
+{
+	protected $errorCodes;
 
-
-class ErrorController extends Controller {
-
-    protected $data = array();  
-    protected $errorCodes;  	
-		 
-  
 	public function __construct() {
 		$this->errorCodes = array(
 			"1001" => "Jeej category ERROR_SOURCE_TITLE does not exists!",
 			"1002" => "Ouch... account ERROR_SOURCE_TITLE does not exists!",
-			"1003" => "Whoops... folder ERROR_SOURCE_TITLE does not exists!",			
-			"1004" => "jejejejej... presentation ERROR_SOURCE_TITLE is missing!",						
-		);					
-	}                                      
-                                                  
+			"1003" => "Whoops... folder ERROR_SOURCE_TITLE does not exists!",
+			"1004" => "jejejejej... presentation ERROR_SOURCE_TITLE is missing!",
+		);
+	}
+
     public function newErrorPageAction($errorCode,$errorSourceTitle) {
-		$errorTitle = $this->errorCodes[$errorCode];  
+		$errorTitle = $this->errorCodes[$errorCode];
 		$errorTitle = str_replace("ERROR_SOURCE_TITLE",$errorSourceTitle,$errorTitle);
-		$this->data['errorTitle'] = $errorTitle;
-		return $this->render('SlidesLiveBundle:Error:newErrorPage.html.twig', $this->data);    
-    }             
+		$data['errorTitle'] = $errorTitle;
+
+		$response = $this->render('SlidesLiveBundle:Error:newErrorPage.html.twig', $data);
+		$response->setStatusCode(404);
+		return $response;
+	}
 }

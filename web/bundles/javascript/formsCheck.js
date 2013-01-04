f3c360fcee18c5587564922354edaaafba816b2c
Vojta Ciml <VojtaCiml@gmail.com>
2013-01-04 16:53:06 +0100

    Login pre-validator

---
+++ web/bundles/javascript/formsCheck.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+function initLoginFormCheck(){
+	console.log("Runtime form checking init");
+	$("#username").blur(function(e) {
+		if(!isValidEmail(e.target.value)){
+			$('#username').css("border-color", "#CC9900");		
+			$('label[for=username]').html('You should enter your email ;)\<br \/\>');			
+			$('label[for=username]').css('color','#CC9900');						
+		} else {
+			$('#username').css("border-color", "#000");		
+			$('label[for=username]').html('Username<br \/\>');						
+			$('label[for=username]').css('color','#FFF');			
+		}
+	});
+}
+
+function isValidEmail(emailString){
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(emailString);	
+}

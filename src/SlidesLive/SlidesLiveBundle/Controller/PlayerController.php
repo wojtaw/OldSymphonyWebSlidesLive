c331bf787917070eb6ffc6c2d09367576ac8fd1c
Vojta Ciml <VojtaCiml@gmail.com>
2013-02-25 00:03:50 +0100

    Finding account and presentation, preparing note

--- src/SlidesLive/SlidesLiveBundle/Controller/PlayerController.php
+++ src/SlidesLive/SlidesLiveBundle/Controller/PlayerController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+use SlidesLive\SlidesLiveBundle\Entity\Note;
+use SlidesLive\SlidesLiveBundle\Entity\Account;
+use SlidesLive\SlidesLiveBundle\Entity\Presentation;
+
 class PlayerController extends Controller {
 
     protected $data = array();                                         
@@ -23,17 +27,33 @@ class PlayerController extends Controller {
 	public function addNoteAction(Request $request) {
 		$presentationID = $request->request->get('presentationID');
 		$noteContent = $request->request->get('noteContent');
-		if($presentationID == null)	return new Response('Presentation ID or note content is not included',412);
-		if($noteContent == null) return new Response('Presentation ID or note content is not included',412);
+		$timecode = $request->request->get('timecode');		
+		if($presentationID == null)	return new Response('Presentation ID is not included',412);
+		if($noteContent == null) return new Response('Note content is not included',412);
+		if($timecode == null) return new Response('Timecode is not included',412);		
 
 		//Check if user is logged, and find what user it is or return 401 CODE
 		$securityContext = $this->container->get('security.context');
 		if( !$securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED') ){
 			return new Response('NOT LOGGED IN',401);				
 		}
-				
-		$user = $this->get('security.context')->getToken()->getUser();
-		echo "User".$user->getId();
+		
+		//Find account and presentation
+		$em = $this->getDoctrine()->getEntityManager();	
+		
+		$accountId = $this->get('security.context')->getToken()->getUser()->getId();
+		
+		$account = $em->getRepository('SlidesLiveBundle:Account')->find($accountId);
+		
+		echo $account->getUsername();
+		//Create new note with content
+		$note = new Note();
+		$note->setTimecode($timecode);
+		$note->setTextContent($noteContent);	
+		
+		
+		//Save stuff to database
+		
 		
 		//Find the presentation
 		
